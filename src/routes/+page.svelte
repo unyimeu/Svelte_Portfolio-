a29d4fc5c247786690201a7da13aac172bcc3079
Unyi Usua <unyime@mit.edu>
2026-02-24 14:08:12 -0500

    Added Svelte funcitonality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav class="nav">
+  <ol>
+    <li class="nav"><a class="current" href="./">Home</a></li>
+    <li class="nav"><a href="projects">Projects</a></li>
+    <li class="nav"><a href="contact">Contact</a></li>
+    <li class="nav">
+      <a href="https://github.com/unyimeu" target="_blank">GitHub Profile</a>
+    </li>
+    <li class="nav"><a href="resume">Resume</a></li>
+  </ol>
+</nav>
+
+<h1>Unyimeabasi Usua</h1>
+<p>
+  Hi, my name is Unyi. I am a junior at MIT studying Artificial Intelligence and
+  Decision Making. This summer I will be interning as a SWE @ Adobe on their
+  Digital Experience Data Insights Team.
+  <img src="images/drinking boba.jpg" alt="Unyimeabasi Usua" width="600" />
+</p>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,34 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+
+<nav class="nav">
+  <ol>
+    <li class="nav"><a href="../">Home</a></li>
+    <li class="nav"><a href="../projects">Projects</a></li>
+    <li class="nav"><a class="current" href="./">Contact</a></li>
+    <li class="nav">
+      <a href="https://github.com/unyimeu" target="_blank">GitHub Profile</a>
+    </li>
+    <li class="nav"><a href="../resume">Resume</a></li>
+  </ol>
+</nav>
+<h1>Contact</h1>
+<form action="mailto:unyime@mit.edu" method="get">
+  <label>
+    Subject:
+    <input type="text" name="subject" id="subject" required />
+  </label>
+
+  <label>
+    Enter your email:
+    <input type="email" name="email" id="email" required />
+  </label>
+
+  <label>
+    Body:
+    <textarea id="body" name="body"></textarea>
+  </label>
+
+  <button type="submit">Send</button>
+</form>
